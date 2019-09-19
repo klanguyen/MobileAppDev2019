a9827fca2185c423bbc9e43130316e8985af4e26
klanguyen <nnguyen1@my.wctc.edu>
2019-09-18 20:50:16 -0500

    ThreeButtonsApp Project

---
+++ ThreeButtonsApp/ThreeButtonsApp.Android/obj/Debug/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: f02295cd8229197d2f705f230d635b7e798609b0 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1491
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 390643
+	.include	"typemap.mj.inc"
---
+++ ThreeButtonsApp/ThreeButtonsApp/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ThreeButtonsApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+		public String dot = ".";
+		public String dash = "-";
+        public string dotndashes = "";
+
+		void OnDotButtonClicked(object sender, EventArgs args)
+		{
+            Button button = (Button)sender;
+            input.Text = string.Concat(input.Text, dot);
+		}
+
+		void OnDashButtonClicked(object sender, EventArgs args)
+		{
+			Button button = (Button)sender;
+
+			input.Text = string.Concat(input.Text, dash);
+		}
+
+		void OnSpaceButtonClicked(object sender, EventArgs args)
+		{
+			Button button = (Button)sender;
+			output.Text = MorseCoder(input.Text).ToString().Trim();
+            input.Text = "Input: ";
+
+        }
+
+		private static String[] codes = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "" };
+
+		private static char[] letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ' };
+
+		public static char MorseCoder(string code)
+		{
+			char result = '?';
+            code = code.Substring(7);
+			for (int i = 0; i < codes.Length; i++)
+			{
+				if (codes[i].Equals(code))
+				{
+					result = letters[i];
+					break;
+				}
+			}
+			return result;
+		}
+	}
+}
